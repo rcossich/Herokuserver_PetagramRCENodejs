658c7e60ffe1374489c4c8ef59aa56b6da064242
rcossich <ricardo.cossich@gmail.com>
2017-04-18 15:31:45 -0600

    Conexion Base Datos FireBase

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var firebase = require("firebase");
+
+firebase.initializeApp({
+  serviceAccount: "PetragramRCENodejs-6812688ab89d.json",
+  databaseURL: "https://petragramrcenodejs.firebaseio.com"
+});
+
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -20,13 +27,47 @@ app.get('/', function(request, response) {
 //POST para registrar usuario (manda id del dispositivo y el id de usuario de instragram.)
 //https://whispering-cliffs-37590.herokuapp.com/registrar-usuario
 //id_dispositivo,id_usuario_instagram
-app.post("/registrar-usuario", function(request,response) {
+var registrarUsuarioURI = "registrar-usuario";
+app.post("/" + registrarUsuarioURI, function(request,response) {
 	response.write(request.body.id_dispositivo);
 	response.write("\n");
 	response.write(request.body.id_usuario_instagram);
 	response.end();
+	var id_dispositivo 	= request.body.id_dispositivo;
+	var id_usuario_instagram 	= request.body.id_usuario_instagram;
+	var db = firebase.database();
+	var registro = db.ref(registrarUsuarioURI).push();
+	registro.set({
+		id_dispositivo : id_dispositivo,
+		id_usuario_instagram : id_usuario_instagram
+	});	
+
+
+	var path = registro.toString(); 
+	var pathSplit = path.split(registrarUsuarioURI + "/")
+	var idAutoGenerado = pathSplit[1];
+	var respuesta = generarRespuestaAToken(db, idAutoGenerado);
+	response.setHeader("Content-Type", "application/json");
+	response.send(JSON.stringify(respuesta));
 }
 );
+
+function generarRespuestaAToken(db, idAutoGenerado) {
+	var respuesta = {};
+	var usuario = "";
+	var ref = db.ref(registrarUsuarioURI);
+	ref.on("child_added", function(snapshot, prevChildKey) {
+		usuario = snapshot.val();
+		respuesta = {
+			id: idAutoGenerado,
+			id_dispositivo : usuario.id_dispositivo,
+			id_usuario_instagram : usuario.id_usuario_instagram
+		};
+
+	});
+	return respuesta;
+}
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
