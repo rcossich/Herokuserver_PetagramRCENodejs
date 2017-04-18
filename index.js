7ece650d981f5d491e4e464f7552cde085b1cf45
rcossich <ricardo.cossich@gmail.com>
2017-04-18 11:17:44 -0600

    registrar-usuario metodo POST

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -12,6 +16,15 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+//POST para registrar usuario (manda id del dispositivo y el id de usuario de instragram.)
+//https://whispering-cliffs-37590.herokuapp.com/registrar-usuario
+//id_dispositivo,id_usuario_instagram
+app.post("/registrar-usuario", function(request,response) {
+	response.send(request.body.id_dispositivo);
+	response.send(request.body.id_usuario_instagram);
+}
+);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
