36cfd8558259b54664aabc5e253896d365fa7d6d
rcossich <ricardo.cossich@gmail.com>
2017-04-18 11:36:56 -0600

    registrar-usuario metodo POST

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ app.get('/', function(request, response) {
 //https://whispering-cliffs-37590.herokuapp.com/registrar-usuario
 //id_dispositivo,id_usuario_instagram
 app.post("/registrar-usuario", function(request,response) {
-	response.write(request.body.id_dispositivo);
-	response.write(request.body.id_usuario_instagram);
+	response.send(request.body.id_dispositivo,'\n',request.body.id_usuario_instagram);
 }
 );
 
