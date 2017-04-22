75ed2f455e278dff0df5be6961e32f4ee9fe9290
rcossich <ricardo.cossich@gmail.com>
2017-04-22 15:23:20 -0600

    POST registra_like etapa1

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ firebase.initializeApp({
   databaseURL: "https://petragramrcenodejs.firebaseio.com"
 });
 
+
+var FCM = require('fcm-push');  //habilitar el FireBase Cloud Messagging push notification.
+
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -63,6 +66,55 @@ function generarRespuestaAToken(db, idAutoGenerado) {
 	});
 	return respuesta;
 }
+
+
+
+//POST para registrar un like desde el TimeLine de la aplicacion (manda id owner, id media y id sender).
+// el owner es el id de instagram del dueño de la cuenta de instagram donde pertenece la media.
+// el id media es el identificador de isntagram para la media a la que se le dio like en el TimeLine.
+// el id sender es el id de instagram definido como principal en la aplicacion.
+// este id owner debe estar registrado en registrar-usuario para poder enviar notificacion.
+//https://whispering-cliffs-37590.herokuapp.com/registrar-like
+//id_owner_instagram, id_media_instagram,id_sender_instagram
+var registrarlikeURI = "registrar-like";
+app.post("/" + registrarlikeURI, function(request,response) {
+	var id_owner_instagram  	= request.body.id_owner_instagram;
+	var id_media_instagram  	= request.body.id_media_instagram;
+	var id_sender_instagram 	= request.body.id_sender_instagram;
+	var existia_like = false;
+	var mensaje = null;
+
+//verificar si existe otro like con (id_owner_instagram,id_media_instagram,id_sender_instagram)
+//sino hay codigo es porque no se decidio implementar.
+	
+//insertando en FireBase el Like.
+	var db = firebase.database();
+	var registro = db.ref(registrarlikeURI).push();
+	registro.set({
+		id_owner_instagram : id_owner_instagram,
+		id_media_instagram : id_media_instagram,
+		id_sender_instagram : id_sender_instagram
+
+	});	
+
+	//verificando si existe registrado como usuario el id del owner de la media.
+	var busqueda = db.ref(registrarUsuarioURI);
+	var variable_busqueda = "id_usuario_instagram";
+	busqueda.orderByChild(variable_busqueda).equalTo(id_owner_instagram).on("value"), function(snapshot){
+		console.log(snapshot.key + " del id usuario"+ snpashot.val().id_usuario_instagram+ " con dispositivo "+snapshot.val().id_dispositivo);
+	}, function(errorObject){
+		console.log("Hubo un error: "+errorObject.code);
+	}
+
+	//var path = registro.toString(); 
+	//var pathSplit = path.split(registrarUsuarioURI + "/");
+	//var idAutoGenerado = pathSplit[1];
+	//var respuesta = generarRespuestaAToken(db, idAutoGenerado);
+	//response.setHeader("Content-Type", "application/json");
+	//response.send(JSON.stringify(respuesta));
+}
+);
+
 
 
 app.listen(app.get('port'), function() {
